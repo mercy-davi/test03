b82616372d2ea75c3ea4036d1a6cbcdca786ba64
小威 <hedawei0816@126.com>
2021-04-15 10:30:23 +0800

    init

---
+++ src/test/java/com/example/labelscore/LabelScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labelscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabelScoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
